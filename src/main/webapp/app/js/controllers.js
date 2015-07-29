fc6b8ea1c7fcafa295b2afbe760558cdf729bafb
VBorikic <vlada.borikic@gmail.com>
2015-07-29 12:06:37 +0200

    vracen slucajno izbrisani kontroler :D

--- src/main/webapp/app/js/controllers.js
+++ src/main/webapp/app/js/controllers.js
@@ -8,6 +8,13 @@ controllers.controller('knjigaController', function($scope, $location, $http,
 	console.log('knjige');
 	console.log($scope.knjige);
 	
+// $scope.obrisiKnjigu = function(knjiga) { // Delete a movie. Issues a DELETE
+// to /api/movies/:id
+// knjiga.$delete(function() {
+// // on success
+// alert('Knjiga je obrisana!');
+// }
+// };
 	$scope.obrisiKnjigu = function(knjiga){
 		$http.delete('service/knjige/'+knjiga.isbn).
 		 success(function() {
@@ -20,14 +27,27 @@ controllers.controller('knjigaController', function($scope, $location, $http,
 	}
 });
 
+controllers.controller('knjigaDetaljiController', function($scope, $location, $routeParams,
+			knjigaService) {
+		$scope.isbn = $routeParams.isbn;
+		console.log('trazim knjigu');
+		$scope.knjiga = knjigaService.get({isbn:$scope.isbn}, function (){
+			console.log('vracena knjiga');
+			});
+});
+
 controllers.controller('knjigaNovaController', function($scope, $location,
 		knjigaService, izdavaciService, autoriService) {
-	  $scope.knjiga = new knjigaService();  //create new movie instance. Properties will be set via ng-model on UI
-	  //posalji zahtev za liste autora i izdavaca
+	  $scope.knjiga = new knjigaService();  // create new movie instance.
+											// Properties will be set via
+											// ng-model on UI
+	  // posalji zahtev za liste autora i izdavaca
 	  $scope.autori = autoriService.query();
 	  $scope.izdavaci = izdavaciService.query();
 	  
-	  $scope.dodajKnjigu = function() { //create a new movie. Issues a POST to /api/movies
+  
+	  $scope.dodajKnjigu = function() { // create a new movie. Issues a POST to
+										// /api/movies
 	    $scope.knjiga.$save(function() {
 	      // on success
 	    	alert('Knjiga je sacuvana!');
